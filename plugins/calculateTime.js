012f4762f5fe22b1ac7e6eddbd49d53ebb9d6eab
xuxiaowengit <531556397@qq.com>
2024-12-09 12:54:11 +0800

    20241209

--- plugins/calculateTime.js
+++ plugins/calculateTime.js
@@ -1,1 +1,1 @@
-function calculateProcessTime(e){const c=3.5*e;if(e<1500){return`${Math.ceil(c/60)} 分钟`}return`${(c/60/60).toFixed(2)} 小时`}module.exports=calculateProcessTime;
+function calculateProcessTime(e){const t=3.5*e;if(e<=10){return`${Math.ceil(t)} 秒钟`}if(e>10&&e<1500){return`${Math.ceil(t/60)} 分钟`}return`${(t/60/60).toFixed(2)} 小时`}module.exports=calculateProcessTime;
